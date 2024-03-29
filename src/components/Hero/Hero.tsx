a2227ec46c23d397d3a53d9b02779392241ea323
FabianReitz <reitz@stadtwerk.org>
2024-03-29 18:43:06 +0100

    💄 Fixed missing whitespace

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -12,7 +12,7 @@ export const Hero = () => {
           <h1 className='text-6xl font-bold text-center text-white'>
             Hi, I'm <span className='text-[#e91e62]'>Fabian</span>!
           </h1>
-          <p className='pt-10 text-2xl text-center text-white leading-relaxed mx-7'>
+          <p className='pt-10 text-2xl text-center text-white leading-relaxed mx-7 mb-12'>
             I'm a{' '}
             <span className='text-[#e91e62] text-3xl'>
               Fullstack Web Developer
