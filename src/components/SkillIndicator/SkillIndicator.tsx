3199f984842444ef9f5e2e0c4c79460c082adbd0
FabianReitz <reitz@stadtwerk.org>
2024-04-03 22:23:19 +0200

    🐛 Fixed overflow

--- src/components/SkillIndicator/SkillIndicator.tsx
+++ src/components/SkillIndicator/SkillIndicator.tsx
@@ -4,7 +4,7 @@ interface SkillIndicatorProps {
 
 export const SkillIndicator = (props: SkillIndicatorProps) => {
   return (
-    <div className='flex bg-slate-300 rounded-full px-1.5'>
+    <div className='flex bg-slate-300 rounded-full px-1.5 w-48'>
       <div className='pixel-font text-xs mr-1 text-blue-700'>EXP</div>
       <div className='rounded-full w-40 h-2 my-auto bg-black bg-opacity-45'>
         <div
