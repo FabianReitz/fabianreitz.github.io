bb892b79b296d28fc8b29b2ce20989be87cf2395
FabianReitz <reitz@stadtwerk.org>
2024-04-03 23:24:15 +0200

    🐛 Fix overflow again

--- src/components/SkillsCard/SkillsCard.tsx
+++ src/components/SkillsCard/SkillsCard.tsx
@@ -10,9 +10,9 @@ interface SkillsCardProps {
 
 export const SkillsCard = (props: SkillsCardProps) => {
   return (
-    <div className='h-[64px] w-[256px] bg-[#e91e62] bg-opacity-75 rounded-lg flex'>
+    <div className='h-16 w-64 bg-[#e91e62] bg-opacity-75 rounded-lg flex'>
       <div className='my-auto mx-4'>{props.image}</div>
-      <div className='my-auto flex flex-col'>
+      <div className='my-auto flex flex-col w-44'>
         <h4 className='text-xl my-auto pb-1'>{props.title}</h4>
         <SkillIndicator skillLevelPercent={props.skillLevelPercent} />
       </div>
