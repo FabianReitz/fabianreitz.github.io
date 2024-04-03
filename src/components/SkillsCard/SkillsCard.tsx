b3f1f4e34e2a9508c215324c1fb094e49123ca85
FabianReitz <reitz@stadtwerk.org>
2024-04-03 23:29:44 +0200

    🐛 Still fixing overflow

--- src/components/SkillsCard/SkillsCard.tsx
+++ src/components/SkillsCard/SkillsCard.tsx
@@ -10,7 +10,7 @@ interface SkillsCardProps {
 
 export const SkillsCard = (props: SkillsCardProps) => {
   return (
-    <div className='h-16 w-64 bg-[#e91e62] bg-opacity-75 rounded-lg flex'>
+    <div className='h-16 w-72 bg-[#e91e62] bg-opacity-75 rounded-lg flex'>
       <div className='my-auto mx-4'>{props.image}</div>
       <div className='my-auto flex flex-col w-44'>
         <h4 className='text-xl my-auto pb-1'>{props.title}</h4>
